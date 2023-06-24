71b93ce3f6b19104f641cca07ef0579eaeef90ea
Шемаев Кирилл <shemaev.kirill@yandex.ru>
2023-06-24 12:50:50 +0300

    add comments to properties of CaseStagePoll

--- Simulator/Simulator/Models/CaseStage/CaseStagePoll.cs
+++ Simulator/Simulator/Models/CaseStage/CaseStagePoll.cs
@@ -4,14 +4,31 @@ namespace Simulator.Models
 {
     internal class CaseStagePoll : CaseStage
     {
+        // Множественный выбор или нет?
         public bool ManyAnswers { get; set; }
+        
+        // Варианты ответов
         public List<string> Options { get; set; }
+
+        // Переход на следующий стейдж зависит от ответа или нет?
         public bool ConditionalMove { get; set; }
+
+        // Только при одиночном ответе! Словарь условных переходов. Номер ответа - номер стейджа, на который переходим
         public Dictionary<int, int> MovingNumbers { get; set; }
+
+        // Количество баллов за ответ. Номер ответа - количество баллов
         public Dictionary<int, double> PossibleRate { get; set; }
+
+        // Минус баллов, если ответ не указан. При множественном выборе. Номер ответа - штраф по баллам
         public Dictionary<int, double> NonAnswers { get; set; }
+
+        // Нужно ли заполнять предыдущий список?
         public bool WatchNonAnswer { get; set; }
+
+        // Лимит ответов (при множественном выборе)
         public int Limit { get; set; }
+
+        // Штраф баллов за каждый ответ при превышенном лимите (при множественном выборе)
         public double Fine { get; set; }
 
         public CaseStagePoll()
