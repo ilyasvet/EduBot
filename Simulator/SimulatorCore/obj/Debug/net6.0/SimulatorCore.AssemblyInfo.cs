cbe6d88408a387653a9daf998a31d6b11df6e20c
Ilya <shemaev.kirill@yandex.ru>
2024-02-21 09:31:45 +0300

    delete useless

--- Simulator/SimulatorCore/obj/Debug/net6.0/SimulatorCore.AssemblyInfo.cs
+++ Simulator/SimulatorCore/obj/Debug/net6.0/SimulatorCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimulatorCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e08fae88945acd8cbd97140885c3f6560ed567")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimulatorCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimulatorCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
